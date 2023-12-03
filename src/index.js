68b4fd1c95b7eda59854f9180bc33d2f6a6a4d8f
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2023-12-02 23:23:27 -0600

    Added express.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import express from 'express'
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send({ msg: 'Hello There' })
+})
+
+app.listen(port)
