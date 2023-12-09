2d6a0e6e479e56fcdc703b79e717199aaeab5fec
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2023-12-08 21:55:58 -0600

    minor changes

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,16 +1,18 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable prettier/prettier */
 import { Router } from 'express'
-// import basicAuth from 'express-basic-auth'
+import basicAuth from 'express-basic-auth'
 
 import recipes from './recipes'
 
 const router = Router()
 
-/* router.use(
+router.use(
   basicAuth({
     users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
   }),
-) */
+)
 
 router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
